1077ae97c586185debc973ab738a2ee5ab2b3a62
vimalkrml <96064908+vimalkrml@users.noreply.github.com>
2022-01-04 17:02:40 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-alert("Hi all !! Click OK & Enjoy Typing-- Vimal");
+alert("Hi all !! This is Vimal . Click OK & Enjoy Typing . Click Play Button for Music in Top Page");
 function play() {
         var audio = document.getElementById("audio");
         audio.play();
