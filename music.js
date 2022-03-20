811e9fc8e60f77b39aeff518c0a1fcd0750267da
vimalkrml <96064908+vimalkrml@users.noreply.github.com>
2022-03-20 23:02:41 +0530

    Delete music.js

--- music.js
+++
@@ -1,5 +0,0 @@
-alert("Hi AK !! This is Vimal . Click OK & Enjoy Typing . Click Play Button for Music (Anbil Avan)");
-function play() {
-        var audio = document.getElementById("audio");
-        audio.play();
-      }
