fe6aa602a8a1e0b6ece60bdf507a1926ebd73905
VIMAL K R <96064908+vimalkrml@users.noreply.github.com>
2022-05-30 23:11:43 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,5 @@
 alert("Website is under developing !! Enjoy Free Demo Sample Version of ML Player");
-prompt("Type Your Fav Song",'VTV');
+prompt("Type Your Fav Song , Will add soon in playlist",'VTV');
 function play() {
         var audio = document.getElementById("audio");
         audio.play();
