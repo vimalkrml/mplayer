893da54238653dff57ff2035726a90c85ba23163
VIMAL K R <96064908+vimalkrml@users.noreply.github.com>
2022-05-30 23:08:57 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-alert("Remix Alert MLB");
+alert("Website is under developing !! Enjoy Free Demo Sample Version of ML Player");
 prompt("Type Your Fav Song",'VTV');
 function play() {
         var audio = document.getElementById("audio");
